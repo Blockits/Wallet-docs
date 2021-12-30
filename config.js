e84e183463cc7b96aaa21a61b37a55926d48fdd0
kevinnguyenai <kevin.nguyen.ai@gmail.com>
2021-12-30 15:45:23 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,81 @@
+const config = {
+  gatsby: {
+    pathPrefix: '/',
+    siteUrl: 'https://github.com/Blockits/Wallet-docs',
+    gaTrackingId: null,
+    trailingSlash: false,
+  },
+  header: {
+    logo: 'https://github.com/Blockits/Wallet-docs/public/static/img/icon-128.png',
+    logoLink: 'https://github.com/Blockits/Wallet-docs',
+    title:
+      "<a href='https://github.com/Blockits/Wallet-docs'><img class='img-responsive' src='https://github.com/Blockits/Wallet-docs/public/static/img/icon-34.png' alt='Wallet logo' /></a>",
+    githubUrl: 'https://github.com/hasura/gatsby-gitbook-boilerplate',
+    helpUrl: '',
+    tweetText: '',
+    social: `<li>
+		    <a href="https://twitter.com/hasurahq" target="_blank" rel="noopener">
+		      <div class="twitterBtn">
+		        <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/twitter-brands-block.svg' alt={'Twitter'}/>
+		      </div>
+		    </a>
+		  </li>
+			<li>
+		    <a href="https://discordapp.com/invite/hasura" target="_blank" rel="noopener">
+		      <div class="discordBtn">
+		        <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/discord-brands-block.svg' alt={'Discord'}/>
+		      </div>
+		    </a>
+		  </li>`,
+    links: [{ text: '', link: '' }],
+    search: {
+      enabled: false,
+      indexName: '',
+      algoliaAppId: process.env.GATSBY_ALGOLIA_APP_ID,
+      algoliaSearchKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
+      algoliaAdminKey: process.env.ALGOLIA_ADMIN_KEY,
+    },
+  },
+  sidebar: {
+    forcedNavOrder: [
+      '/introduction', // add trailing slash if enabled above
+      '/codeblock',
+    ],
+    collapsedNav: [
+      '/codeblock', // add trailing slash if enabled above
+    ],
+    links: [{ text: 'Hasura', link: 'https://hasura.io' }],
+    frontline: false,
+    ignoreIndex: true,
+    title:
+      "<a href='https://hasura.io/learn/'>graphql </a><div class='greenCircle'></div><a href='https://hasura.io/learn/graphql/react/introduction/'>react</a>",
+  },
+  siteMetadata: {
+    title: 'Gatsby Gitbook Boilerplate | Hasura',
+    description: 'Documentation built with mdx. Powering hasura.io/learn ',
+    ogImage: null,
+    docsLocation: 'https://github.com/hasura/gatsby-gitbook-boilerplate/tree/master/content',
+    favicon: 'https://graphql-engine-cdn.hasura.io/img/hasura_icon_black.svg',
+  },
+  pwa: {
+    enabled: false, // disabling this will also remove the existing service worker.
+    manifest: {
+      name: 'Gatsby Gitbook Starter',
+      short_name: 'GitbookStarter',
+      start_url: '/',
+      background_color: '#6b37bf',
+      theme_color: '#6b37bf',
+      display: 'standalone',
+      crossOrigin: 'use-credentials',
+      icons: [
+        {
+          src: 'src/pwa-512.png',
+          sizes: `512x512`,
+          type: `image/png`,
+        },
+      ],
+    },
+  },
+};
+
+module.exports = config;
